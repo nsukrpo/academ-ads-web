f33ec91162d5f5644bfb55d1d8033e75c2e790f1
lerabbb <lerabubl@gmail.com>
2023-11-12 23:49:40 +0700

    made registration info
    width not auto, need to fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from "./components/header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Infobar from "./components/infobar/Infobar";
 import AuthScreen from "./components/auth_screen/AuthScreen";
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Sidebar/>
+      <div className="container row">
+        <Sidebar/>
+        <div className="vertical__line"/>
+        <Infobar/>
+      </div>
     </div>
   );
 }
---
+++ src/components/infobar/Infobar.jsx
@@ -0,0 +1,24 @@
+import './infobar.css';
+import './../../styles/common.css';
+import './../../styles/text.css';
+
+function Infobar() {
+    return (
+        <div className="infobar">
+            <div className="container">
+                <div className="heading__A2">Hey, Admin</div>
+                <div className="infobar__row">
+                    <div className="heading__B1 violet">Latest registration users</div>
+                    <div className="registration__row">
+                        <div className="paragraph__B1 gray">Just now</div>
+                        <div className="heading__C2 secondary user__name">XYZ Name</div>
+                        <div className="heading__C2 secondary">xyz@gmail.com</div>
+                    </div>
+                </div>
+                <hr className="info__line"/>
+            </div>
+        </div>
+    )
+}
+
+export default Infobar;

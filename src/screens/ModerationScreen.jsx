d9b36047c45adf26a4bd3d119190c05eee6a9d05
lyect <lyect1@gmail.com>
2023-12-06 01:14:56 +0700

    Fixed tabulation

--- src/screens/ModerationScreen.jsx
+++ src/screens/ModerationScreen.jsx
@@ -5,19 +5,19 @@ import ComponentList from '../components/component_list/ComponentList';
 import AdvertisementItem from '../components/component_list/AdvertisementItem';
 
 export default function Moderation () {
-    const title = 'Объявления, ожидающие рассмотрения'
-    
-    return (
-      <div>
-        <Header/>
-          <div className="row">
-            <Sidebar/>
-            <div className="vertical__line"/>
-            <div className="column">
-              <Infobar/>
-              { ComponentList(title, AdvertisementItem)}
-            </div>
+  const title = 'Объявления, ожидающие рассмотрения'
+  
+  return (
+    <div>
+      <Header/>
+        <div className="row">
+          <Sidebar/>
+          <div className="vertical__line"/>
+          <div className="column">
+            <Infobar/>
+            { ComponentList(title, AdvertisementItem)}
           </div>
-      </div>
-    )
-  }
+        </div>
+    </div>
+  )
+}
